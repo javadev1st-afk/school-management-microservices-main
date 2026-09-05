bf8c26ed650828d48688e45f1a83da9172e28960
javadev1st-afk <javadev1st@gmail.com>
2026-09-05 22:27:23 +0530

    reverted extra changes from api-gateway module

--- api-gateway/src/main/java/com/school/apigateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/school/apigateway/ApiGatewayApplication.java
@@ -4,15 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.FilterType;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @ComponentScan(
-    basePackages = {"com.school.apigateway", "com.pawan.share.jwt"},
-    excludeFilters = @ComponentScan.Filter(
-        type = FilterType.ASSIGNABLE_TYPE,
-        classes = com.pawan.share.jwt.SecurityConfig.class))
+    basePackages = {"com.school.apigateway", "com.pawan.share.jwt"})
 public class ApiGatewayApplication {
     public static void main(String[] args) {
         SpringApplication.run(ApiGatewayApplication.class, args);
