6fe85456afc51417ac401481b451eb1707589df3
javadev1st-afk <javadev1st@gmail.com>
2026-09-15 12:52:07 +0530

    ID added

--- user-service/src/main/java/com/school/userservice/entity/ClassTeacher.java
+++ user-service/src/main/java/com/school/userservice/entity/ClassTeacher.java
@@ -13,6 +13,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -25,6 +26,7 @@ import lombok.NoArgsConstructor;
 @Builder
 @EntityListeners(AuditingEntityListener.class)
 public class ClassTeacher {
+    @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     @Column(name = "class_id", nullable = false)
